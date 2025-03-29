7e1ff463ff0e53f97f3afe716686293f6af16b6f
wisdomdzontoh <wisdomdzontoh@gmail.com>
2025-03-29 22:50:20 +0000

    fix: use useParams() in EmbedBotPage

--- app/dashboard/embed/[bot_id]/page.tsx
+++ app/dashboard/embed/[bot_id]/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import EmbedPageClient from '../EmbedPageClient';
+"use client"
 
+import React from "react"
+import { useParams } from "next/navigation"
+import EmbedPageClient from "../EmbedPageClient"
 
-export default function EmbedBotPage({ params }: { params: { bot_id: string } }) {
-    return <div><EmbedPageClient chatbotId={Number(params.bot_id)} /></div>;
-  }
+export default function EmbedBotPage() {
+  const params = useParams()
+  const chatbotId = Number(params?.bot_id)
+
+  return <div><EmbedPageClient chatbotId={chatbotId} /></div>
+}
